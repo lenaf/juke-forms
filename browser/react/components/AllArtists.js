9320d269db5a60b0704049b8bc43ffa3df1d7d1b
Lena Forti <lena@Lenas-MacBook-Pro.local>
2017-07-06 18:08:55 -0400

    console logging playlist input

--- browser/react/components/AllArtists.js
+++ browser/react/components/AllArtists.js
@@ -4,32 +4,48 @@ import axios from 'axios';
 
 export default class AllArtists extends Component {
 
-  constructor () {
+  constructor() {
     super();
     this.state = {
-      artists: []
+      artists: [],
+      inputValue: ""
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  componentDidMount () {
+  componentDidMount() {
     axios.get('/api/artists')
       .then(res => res.data)
       .then(artists => this.setState({ artists }));
   }
 
-  render () {
+  handleChange(event) {
+    this.setState({
+      inputValue: event.target.value
+    });
+  }
 
-    const artists = this.state.artists;
+  render() {
+
+    const artists = this.state.artists.filter(artist => artist.name.match(this.state.inputValue));
 
     return (
       <div>
+        <form className="form-group" style={{ marginTop: '20px' }}>
+          <input
+            onChange={this.handleChange}
+            className="form-control"
+            placeholder="Enter artist name"
+          />
+        </form>
         <h3>Artists</h3>
         <div className="list-group">
           {
             artists.map(artist => {
               return (
                 <div className="list-group-item" key={artist.id}>
-                  <Link to={`/artists/${artist.id}`}>{ artist.name }</Link>
+                  <Link to={`/artists/${artist.id}`}>{artist.name}</Link>
                 </div>
               );
             })
---
+++ browser/react/components/NewPlaylist.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+export default class NewPlaylist extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            inputValue: ""
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log("bob?", event.target.value, "state", this.state.inputValue)
+    }
+
+    handleChange(event) {
+        this.setState({
+            inputValue: event.target.value
+        });
+    }
+
+    render() {
+
+        return (
+
+            <div className="well">
+                <form
+                    className="form-horizontal"
+                    onSubmit={this.handleSubmit}
+                >
+                    <fieldset>
+                        <legend>New Playlist</legend>
+                        <div className="form-group">
+                            <label className="col-xs-2 control-label">Name</label>
+                            <div className="col-xs-10">
+                                <input
+                                    onChange={this.handleChange}
+                                    className="form-control"
+                                    type="text"
+                                    value={this.state.inputValue}
+                                />
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <div className="col-xs-10 col-xs-offset-2">
+                                <button type="submit" className="btn btn-success">Create Playlist</button>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+
+        )
+    }
+
+}
